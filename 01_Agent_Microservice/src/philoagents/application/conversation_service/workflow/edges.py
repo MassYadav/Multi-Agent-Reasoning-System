a598014adcaa49d256e480e8e56d9fd2ef3cf244
Manish <mky80094@gmail.com>
2025-12-06 02:36:44 +0530

    FINAL PUSH: Completed MLOps refactor and updated metadata before repository rename.

--- 01_Agent_Microservice/src/philoagents/application/conversation_service/workflow/edges.py
+++ 01_Agent_Microservice/src/philoagents/application/conversation_service/workflow/edges.py
@@ -5,7 +5,6 @@ from langgraph.graph import END
 from philoagents.application.conversation_service.workflow.state import PhilosopherState
 from philoagents.config import settings
 
-
 def should_summarize_conversation(
     state: PhilosopherState,
 ) -> Literal["summarize_conversation_node", "__end__"]:
